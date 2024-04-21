890907c87f1cd2ced3b4c24685cab5741328ee47
sasa-x <sasmithgit@gmail.com>
2024-04-21 09:40:45 +0530

    Thread Start

--- Introduction/src/main/java/Main.java
+++ Introduction/src/main/java/Main.java
@@ -1,2 +1,14 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("MyFirst Thread");
+    }
+}
+
 public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
 }
