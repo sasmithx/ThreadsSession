169240040fcef416b93307d59f4ea259438400cc
sasa-x <sasmithgit@gmail.com>
2024-04-21 10:21:36 +0530

    Thread Extended Using Runnable

---
+++ Runnable Interface/src/main/java/Main.java
@@ -0,0 +1,35 @@
+class One implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread One");
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Two implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread Two");
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        One one = new One();
+        Two two = new Two();
+        one.run();
+        Thread.sleep(1000);
+        two.run();
+    }
+}
