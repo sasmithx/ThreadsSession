ede7e0064737eb7ce59bd08d25424709f58607d2
sasa-x <sasmithgit@gmail.com>
2024-04-21 09:48:01 +0530

    Create instance of MyRunnable in Main2

--- Introduction/src/main/java/Main2.java
+++ Introduction/src/main/java/Main2.java
@@ -1,5 +1,16 @@
+class MyRunnable implements Runnable {
+
+    @Override
+    public void run () {
+        System.out.println("Thread is running.");
+    }
+}
+
 public class Main2 {
     public static void main(String[] args) {
-
+//        Create instance of MyRunnable
+        MyRunnable runnable = new MyRunnable();
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 }
