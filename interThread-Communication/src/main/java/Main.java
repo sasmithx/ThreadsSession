ba37e328ba9d780b6fadd8fd0666dd5845609923
sasa-x <sasmithgit@gmail.com>
2024-04-21 13:36:36 +0530

    InterThread Communication Producer

--- interThread-Communication/src/main/java/Main.java
+++ interThread-Communication/src/main/java/Main.java
@@ -1,11 +1,35 @@
 class Q{
     int num;
     public void put(int num){
+        System.out.println("PUT : " + num);
         this.num = num;
     }
 
     public void get(){
         System.out.println(num);
+    }
+}
+
+class Producer implements Runnable{
+    Q q;
+
+    public Producer(Q q){
+        this.q = q;
+        Thread t1 = new Thread(this,"Producer");
+        t1.start();
+    }
+
+    @Override
+    public void run () {
+        int i=0;
+        while(true){
+            q.put(i++);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
 
