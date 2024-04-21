88b3349bcc0ebe205b9889b7958abf6d4704e35a
sasa-x <sasmithgit@gmail.com>
2024-04-21 13:27:05 +0530

    InterThread Communication put

--- interThread-Communication/src/main/java/Main.java
+++ interThread-Communication/src/main/java/Main.java
@@ -1,3 +1,10 @@
+class Q{
+    int num;
+    public void put(int num){
+        this.num = num;
+    }
+}
+
 public class Main {
     public static void main (String[] args) {
         System.out.println("Hello World");
